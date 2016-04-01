e0c0a9de144491b71d529bf1ff957e7bd9cdd6b2
Rex <chen.cyr@gmail.com>
2016-04-01 21:28:36 +0800

    1. Refactoring emitter. 2. Support define customer rule for emit handler received new params from listener. 3. Support define customer rule for execute after handler called. 4. Support define customer rule for handler emitting.

--- lab.js
+++
@@ -1,9 +0,0 @@
-var express     = require('express');
-var path        = require("path");
-var app         = express();
-
-var router = express.Router();
-var port = process.env.PORT || 8000;
-app.use(express.static(path.join(__dirname, 'html')));
-app.listen(port);
-console.log('Service on port: ' + port);
